a765e5c9c05d90e6f8f3146deb6c157e38b6a5d7
Mathis Detourbet <mathis.detourbet@gmail.com>
2020-05-30 13:02:10 +0200

    final commit

---
+++ ListApp/Controller/DogDetailsViewController.swift
@@ -0,0 +1,31 @@
+//
+//  DogViewController.swift
+//  ListApp
+//
+//  Created by Mathis Detourbet on 18/3/20.
+//  Copyright © 2020 Mathis Detourbet. All rights reserved.
+//
+
+import UIKit
+import Kingfisher
+
+class DogDetailsViewController: UIViewController {
+    
+    var dog: Dog!
+    
+    
+    @IBOutlet weak var dogImageView: UIImageView!
+    @IBOutlet weak var dogNicknameLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // BUILD UI
+        
+        dogNicknameLabel.text = dog.nickname
+        
+        if let url = dog.imageURL {
+            dogImageView.kf.setImage(with: url)
+        }
+    }
+}
